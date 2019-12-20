e3d9dd89ec793fed6b5f3fb41311842f2f3ebc4b
sptkl <caobaiyue@gmail.com>
2019-12-20 16:43:41 -0500

    planning for future functionalities

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 import os
 from flask import Flask, request, jsonify
-from s3_utils import make_client, bucket
+from utils.s3 import make_client, bucket
+from cook import Archiver
 import pandas as pd
 
 app = Flask(__name__)
 client = make_client()
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        key = request.form.get('key', file.filename)
-        acl = request.form.get('acl', 'private')
-        response = client.put_object(
-            ACL=acl,
-            Body=file.read(),
-            Bucket=bucket,
-            Key=key)
-        return jsonify(response)
+    """
+    uploading file to s3
+    """
+    file = request.files['file']
+    key = request.form.get('key', file.filename)
+    acl = request.form.get('acl', 'private')
+    response = client.put_object(
+        ACL=acl,
+        Body=file.read(),
+        Bucket=bucket,
+        Key=key)
+    return jsonify(response)
 
 @app.route('/delete', methods=['DELETE'])
 def delete_file():
+    """
+    deleting file from s3
+    """
     key=request.args['key']
     print(key)
     if request.method == 'DELETE':
@@ -28,6 +34,32 @@ def delete_file():
             Bucket=bucket, 
             Key=key)
         return jsonify(response)
+
+@app.route('/archive', methods=['POST'])
+def archive():
+    """
+    cli version: cook run <recipe>
+    by default, cook to recipe database
+    whenever a table is updated, also 
+    update recipes table, either as a csv
+    or in the database
+    """
+
+@app.route('/migrate', methods=['POST'])
+def migrate():
+    """
+    migrate table from database A to B
+    table_name.version in A 
+    to table_name.version in B
+    """
+
+@app.route('/download', methods=['GET'])
+def download():
+    """
+    download file from given database to 
+    different format using gdal, 
+    including csv, shapfiles, geojson
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
